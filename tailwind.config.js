54581456a56186197ed6eb7c4c076fcc9b17bdec
山田敬汰 <banira0428@gmail.com>
2021-01-16 22:46:17 +0900

    design: テーマ設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "#33b5e5"
+      },
+      white: colors.white,
+    },
     extend: {},
   },
   variants: {
