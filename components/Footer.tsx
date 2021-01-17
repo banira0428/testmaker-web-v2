71f8e2a0f2325c448eab0baead1fe32190021070
山田敬汰 <banira0428@gmail.com>
2021-01-17 09:28:20 +0900

    add: フッターの実装

---
+++ components/Footer.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+interface FooterMenu {
+  title: string
+  link: string
+}
+
+export default function Footer() {
+
+    const items: FooterMenu[] = [
+      {
+        title: "トップページ",
+        link: "/"
+      },
+      {
+        title: "使い方",
+        link: "/guide"
+      },
+      {
+        title: "プライバシーポリシー",
+        link: "/privacy"
+      },
+      {
+        title: "利用規約",
+        link: "/terms"
+      },
+    ]
+
+    return (
+        <footer className="bg-primary p-2">
+          <img src="img/logo_text.png" className="w-1/2"/>
+            <ul>
+              {
+                items.map((it) =>
+                  <li key={it.title} className="mb-1 ml-2">
+                    <Link href={it.link}>
+                      <a className="text-white text-sm">
+                          {it.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              }
+            </ul>
+          <p className="text-sm text-white text-right">
+            ©2021 ke-ta
+          </p>
+        </footer>
+    )
+}
