daaf598d614fb2d006ba8677f2ea1656896df9fc
山田敬汰 <banira0428@gmail.com>
2021-02-06 17:58:30 +0900

    add: CSSの色設定の変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: "#33b5e5"
-      },
-      white: colors.white,
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#33b5e5"
+        },
+        white: colors.white,
+      }
     },
-    extend: {},
   },
   variants: {
     extend: {},
