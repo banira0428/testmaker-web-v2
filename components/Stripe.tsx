817ad5d2224cdf91e24b6e3444a3df400f3bdf91
山田敬汰 <banira0428@gmail.com>
2021-02-07 08:05:16 +0900

    add: ストライプを描画するためのコンポーネント追加

---
+++ components/Stripe.tsx
@@ -0,0 +1,12 @@
+export default function Stripe() {
+
+  const style = {
+    background: "linear-gradient(150deg, #33b5e5, #33b5e5 40%, #fff 40%, #fff 43%, #33b5e5 43%, #33b5e5 46%,#fff 46%, #fff 49%,#33b5e5 49%, #33b5e5);"
+  }
+
+  return (
+    <div>
+      <div className="absolute top-0 bottom-0 right-0 left-0 z-0" style={style}/>
+    </div>
+  )
+}
