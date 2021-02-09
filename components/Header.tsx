04c811dfc36fb67fe9345717d2fab5733c81a529
山田敬汰 <banira0428@gmail.com>
2021-02-09 16:08:22 +0900

    design: ヘッダーからMaterial-uiを剥がす

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,3 @@
-import AppBar from "@material-ui/core/AppBar/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from '@material-ui/icons/Menu';
 import Link from "next/link";
 import {Component} from "react";
 import Transition from "react-transition-group/cjs/Transition";
@@ -10,30 +7,33 @@ interface HeaderState {
   isShowMenu: boolean
 }
 
-export default class Header extends Component<any,HeaderState> {
+export default class Header extends Component<any, HeaderState> {
 
   constructor(props) {
     super(props);
-    this.state = { isShowMenu: false };
+    this.state = {isShowMenu: false};
   }
 
   render() {
     return (
-      <AppBar position="sticky" elevation={0}>
-        <Toolbar className="bg-primary">
+      <header className="sticky bg-primary z-50">
+        <div className="flex mx-3">
           <Link href="/">
             <a>
               <img src="/img/logo_text.webp" className="h-12" alt={"暗記メーカー"}/>
             </a>
           </Link>
-          <button className="focus:outline-none lg:hidden ml-auto mr-0" aria-label={'メニューボタン'} onClick={() => {
+          <div className="flex-grow"/>
+          <button className="focus:outline-none lg:hidden" aria-label={'メニューボタン'} onClick={() => {
             this.setState({
               isShowMenu: !this.state.isShowMenu
             })
           }}>
-            <MenuIcon/>
+            <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
           </button>
-        </Toolbar>
+        </div>
         <Transition
           in={this.state.isShowMenu}
           timeout={200}>
@@ -63,7 +63,7 @@ export default class Header extends Component<any,HeaderState> {
             }
           }
         </Transition>
-      </AppBar>
+      </header>
     )
   }
 }
