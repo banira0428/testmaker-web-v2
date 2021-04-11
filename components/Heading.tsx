dbcc0da5217084051d9f235eb9274a7bd6b13aa3
ke-ta <banira0428@gmail.com>
2021-04-11 21:55:24 +0900

    使い方説明の文言を追加

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,8 +1,8 @@
 export default function Heading({title, subTitle}) {
   return (
     <div className="py-3 mt-5">
-      <p className="text-5xl md:text-6xl font-bold  mr-auto ml-0">{ title }</p>
-      <h2 className="mr-auto md:text-xl mt-2 ml-0 rounded-b">{ subTitle }</h2>
+      <h2 className="text-5xl md:text-6xl font-bold  mr-auto ml-0">{ title }</h2>
+      <p className="mr-auto md:text-xl mt-2 ml-0 rounded-b">{ subTitle }</p>
     </div>
   )
 }
--- pages/howto/edit_csv.tsx
+++ pages/howto/edit_csv.tsx
@@ -10,15 +10,40 @@ export default function EditCSV() {
       </Head>
       <Layout>
         <div className="mx-auto max-w-5xl p-3">
-          {/* p タグで段落を表示 */}
-          <p>
-            文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章
-          </p>
+        <Heading title="HowTo" subTitle="使い方 / 問題集をまとめて編集"/>
+        <p className="mt-6">このページでは、表計算ツールを用いて問題集をまとめて編集する方法について解説します。なお、このページの操作手順は、PCから行うことを想定しています。</p>
 
-          {/* img タグで画像を表示。 src 属性でファイルの場所を指定する（つまり、撮影したスクショは自分で testmaker_web_v2/public/img 内に配置しておく必要がある） */}
-          <img src="/img/logo_text.webp" />
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">1. テンプレートのコピー</h3>
+        <p className="mt-3"><a className="text-primary" href="https://docs.google.com/spreadsheets/d/1FjuuQKduDykY6S88NqJbAGJGjzLOF-jQlXlG_334_3k/edit?usp=sharing" download>こちら</a>のページにアクセスし、入力例となる表が閲覧できることを確認してください</p>
+        {/* 入力例の表が映っているページのスクショ */}
 
-          {/* <p> や <img> 以外にもいろいろタグがあるので、必要に応じてググるなり質問するなりしてもらえると */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">2. スプレッドシートへのログイン</h3>
+        <p className="mt-6">ログインしていない場合、右上の「ログイン」ボタンをクリックし、ログインしてください</p>
+        {/* スプレッドシート右上の「ログイン」ボタンを示すスクショ */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">3. 入力例の複製</h3>
+        <p className="mt-6">「ファイル」→「コピーを作成」をクリックし、入力例をコピーして編集できる様にしてください</p>
+        {/* 手順がわかる様なスクショ */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">4. 問題情報の入力</h3>
+        <p className="mt-6">入力例を参考にしつつ、各自で問題集の情報を編集してください</p>
+        {/* スプレッドシートに色々入力している時のスクショ */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">5. エクスポート</h3>
+        <p className="mt-6">「ファイル」→「ダウンロード」→「カンマ区切りの値」をクリックし、入力したデータをCSVファイルとして保存してください</p>
+        {/* 手順がわかる様なスクショ */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">6. ファイルの移動</h3>
+        <p className="mt-6">作成したCSVファイルを何かしらの手段でスマートフォンに移動させてください（例：Googleドライブ、Dropbox）</p>
+        {/* 手順がわかる様なスクショ */}
+
+        <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">6. 「暗記メーカー」へのインポート</h3>
+        <p className="mt-6">暗記メーカーを起動し、サイドメニューの「ファイルのインポート」から、先ほど移動させたファイルを選択することで、問題集としてインポートすることができます</p>
+        {/* 手順がわかる様なスクショ */}
+
+
+
         </div>
       </Layout>
     </div>
