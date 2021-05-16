8ee798aac0a91fe4687bd1aa63ba17c50cdd4f66
sakura <cherrymasya196926@gmail.com>
2021-05-16 23:41:12 +0900

    画像のトリミングちょきちょき

--- pages/howto/edit_csv.tsx
+++ pages/howto/edit_csv.tsx
@@ -20,12 +20,12 @@ export default function EditCSV() {
           <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">2. スプレッドシートへのログイン</h3>
           <p className="mt-6">ログインしていない場合、右上の「ログイン」ボタンをクリックし、ログインしてください</p>
           {/* スプレッドシート右上の「ログイン」ボタンを示すスクショ */}
-          <img src="/img/.jpg"/>
+          
             <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">3. 入力例の複製</h3>
           <p className="mt-6">「ファイル」→「コピーを作成」をクリックし、入力例をコピーして編集できる様にしてください</p>
-          <img src="/img/Inkededit_csv3-1_LI.jpg"/>
+          <img src="/img/edit_csv3-1.jpg"/>
           <div className="mt-6"></div>
-          <img src="/img/Inkededit_csv3-2_LI.jpg"/>
+          <img src="/img/Inkededit_csv3-2-2_LI.jpg"/>
 
           <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">4. 問題情報の入力</h3>
           <p className="mt-6">入力例を参考にしつつ、各自で問題集の情報を編集してください</p>
@@ -33,9 +33,9 @@ export default function EditCSV() {
 
           <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">5. エクスポート</h3>
           <p className="mt-6">「ファイル」→「ダウンロード」→「カンマ区切りの値」をクリックし、入力したデータをCSVファイルとして保存してください</p>
-          <img src="/img/Inkededit_csv5-1-2_LI.jpg"/>
+          <img src="/img/edit_csv5-1-2.jpg"/>
           <div className="mt-6"></div>
-          <img src="/img/Inkededit_csv5-2_LI.jpg"/>
+          <img src="/img/edit_csv5-2.jpg"/>
 
           <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">6. ファイルの移動</h3>
           <p className="mt-6">作成したCSVファイルを何かしらの手段でスマートフォンに移動させてください（例：Googleドライブ、Dropbox）</p>
@@ -43,7 +43,7 @@ export default function EditCSV() {
 
           <h3 className="text-3xl md:text-3xl font-bold mr-auto ml-0 mt-6">7. 「暗記メーカー」へのインポート</h3>
           <p className="mt-6">暗記メーカーを起動し、サイドメニューの「ファイルのインポート」から、先ほど移動させたファイルを選択することで、問題集としてインポートすることができます</p>
-          <img src="/img/Inkededit_csv6-1_LI.jpg"/>
+          <img src="/img/Inkededit_csv7-1_LI.jpg"/>
 
 
 
